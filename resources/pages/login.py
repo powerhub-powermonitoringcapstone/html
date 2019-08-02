1d4602360dea296624dad2c867b96898dcf780eb
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-02 11:39:32 +0800

    Groundwork laid for login

--- resources/pages/login.py
+++ resources/pages/login.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-# enable debugging
 #powerhub intro authenticator code (c)2019
-import cgitb, os, sys
+import cgitb, os, sys, cgi
 sys.path.insert(1, '/home/capstone/codebase/')
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
 import graphingHandler as gh
 import settingsHandler as sh
-static = open(cwd + "/intro.html", 'r')
+##static = open(cwd + "/intro.html", 'r')
 cgitb.enable()
 v=230
 a=10
 w=v*a
+form = cgi.FieldStorage()
 print("Content-Type: text/html;charset=utf-8\n")
-print()
-for data in static.readlines():
-    print(data.rstrip('\n'))
+print(form.getvalue('auth'))
+##for data in static.readlines():
+    ##print(data.rstrip('\n'))
