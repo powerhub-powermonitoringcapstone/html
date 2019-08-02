fffdc48ad0106814cc16163423034eb329ba642f
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-02 13:29:47 +0800

    Login update

--- resources/pages/login.py
+++ resources/pages/login.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-# enable debugging
-import cgitb, cgi
+import cgitb, cgi, hashlib
 cgitb.enable()
 form = cgi.FieldStorage()
+form = form.getvalue('auth')
+form = form.encode('utf-8')
+fgt = cgi.FieldStorage()
+fgt = fgt.getvalue('fgt')
+fgt = fgt.encode('utf-8')
+form = hashlib.sha256(form)
+form = form.hexdigest()
+fgt = hashlib.sha256(fgt)
+fgt = fgt.hexdigest()
 print("Content-Type: text/html;charset=utf-8\n\n")
 print()
-print(form.getvalue('auth'))
+print(form + fgt)
 
