0b1d26ab8ad51b3a346a7435a0ec98319c7ee78e
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-03 08:58:17 +0800

    Hashing and salting implemented

--- resources/pages/login.py
+++ resources/pages/login.py
@@ -7,12 +7,16 @@ form = form.getvalue('auth')
 form = form.encode('utf-8')
 fgt = cgi.FieldStorage()
 fgt = fgt.getvalue('fgt')
-fgt = fgt.encode('utf-8')
+fgt = fgt.encode('utf-8') ##fingerprint as salt
+form = form + fgt
+
 form = hashlib.sha256(form)
 form = form.hexdigest()
-fgt = hashlib.sha256(fgt)
-fgt = fgt.hexdigest()
+form = str(form)
+fgt = str(fgt).lstrip('b')
+##fgt = hashlib.sha256(fgt)
+##fgt = fgt.hexdigest()
 print("Content-Type: text/html;charset=utf-8\n\n")
 print()
-print(form + fgt)
+print(form + "<br>" + "Salt:" + fgt)
 
