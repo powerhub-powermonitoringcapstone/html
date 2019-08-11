aeae19646b3ed32daec856b012cedf31684211a9
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-11 20:17:57 +0800

    Auto-login when remembered implemented.

--- resources/pages/wsgi_bin/fuck.py
+++ resources/pages/wsgi_bin/fuck.py
@@ -1,16 +1,18 @@
-##import sys
 import flask as F
-##sys.path.insert(1, '/home/capstone/codebase')
-##import loginHandler as lh
-##cwd = '/home/capstone/html/resources/pages'
-##cwdf = '/home/capstone/codebase'
+cwd = '/home/capstone/html/resources/pages'
+cwdf = '/home/capstone/codebase'
 #sett = open(cwdf + '/pvt.xml', 'r')
 #login = open(cwd + '/login.html', 'r')
 app = F.Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def post():
-    ##fgt = F.request.args.get('test')
-    fgt = F.request.get_json().get('test')
-    return ("Duck {}".format(fgt))
+    import sys
+    sys.path.insert(1, '/home/capstone/codebase')
+    import loginHandler as lh
+    fgt = F.request.get_json().get('fgt')
+    if (lh.isLogin(fgt)):
+        return ("True")
+    else:
+        return ("False")
 if __name__ == "__main__":
     app.run()
