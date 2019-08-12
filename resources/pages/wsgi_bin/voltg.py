ab953fbba728b7f2daf9691cf1c203939895efe0
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-12 18:45:18 +0800

    Fixed minor bugs regarding client.js's need to be placed in the body of the HTML document. Also implemented 'dynamic' voltage

--- resources/pages/wsgi_bin/voltg.py
+++ resources/pages/wsgi_bin/voltg.py
@@ -7,31 +7,6 @@ app = F.Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def post():
     return "230"##F.jsonify(voltage='230')
-##    import sys
-##    sys.path.insert(1, '/home/capstone/codebase')
-##    import loginHandler as lh
-##    if (lh.isLogin(F.request.json.get('fgt'))):
-##        return ("True")
-##    else:
-##        return ("False")
-##@app.route("/auth/", methods=['GET', 'POST'])
-##def auth():
-##    import sys
-##    sys.path.insert(1, '/home/capstone/codebase')
-##    import os, xml.etree.ElementTree as ET, loginHandler as lh, hashlib
-##    data = F.request.json
-##    cwdf = '/home/capstone/codebase'
-##    sett = open(cwdf + '/pvt.xml', 'r')
-##    settings = ET.parse(sett)
-##    root = settings.getroot()
-##    key = str(root.find(".private").attrib['key']) ##end of loading local files
-##    auth = str(data.get('auth')) + str(data.get('salt'))
-##    auth = str(hashlib.sha256(auth.encode('utf-8')).hexdigest())
-##    passh = str(hashlib.sha256((key+str(data.get('salt'))).encode('utf-8')).hexdigest()) #password hashed
-##    if (auth == passh):
-##        lh.newLogin(str(data.get('fgt')))
-##        return ("True")
-##    else:
-##        return ("False")
+
 if __name__ == "__main__":
     app.run()
