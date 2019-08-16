981d30e483c983eca8c68b802c39eb76e34266d2
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-16 20:37:08 +0800

    Working on data auto refresh handler.

--- resources/pages/dynamic.js
+++ resources/pages/dynamic.js
@@ -1,1 +1,16 @@
 //auto data refresh handler (c) 2019 powerhub
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+	while (1){
+		await sleep(1000);
+	}
+}
+
+
+self.addEventListener('message', function() {
+
+  self.postMessage(e.data);
+}, false);
