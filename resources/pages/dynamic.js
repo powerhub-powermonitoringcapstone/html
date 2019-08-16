329405ad6e4e6346bfcac947bda42112b3e87b28
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-16 20:39:27 +0800

    Working on data auto refresh handler.

--- resources/pages/dynamic.js
+++ resources/pages/dynamic.js
@@ -4,7 +4,17 @@ function sleep(ms) {
 }
 
 async function demo() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("demo").innerHTML =
+			this.responseText;
+		}
+	};
+	xhttp.open("GET", "ajax_info.txt", true);
+	xhttp.send(); 
 	while (1){
+
 		await sleep(1000);
 	}
 }
