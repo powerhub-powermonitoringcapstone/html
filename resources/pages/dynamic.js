e0e553a1297793eafe7e1b28db96d9947b8fce6b
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-18 22:35:48 +0800

    Added authentication verification to main page.

--- resources/pages/dynamic.js
+++ resources/pages/dynamic.js
@@ -3,7 +3,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function demo() {
+async function demo(e) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -11,9 +11,13 @@ async function demo() {
 		}
 	};
 	while (1){
-		xhttp.open("GET", "/wsgi_bin/data/", true);
-		xhttp.send(); 
+		xhttp.open("POST", "/wsgi_bin/data/", true);
+		xhttp.setRequestHeader("Content-Type", "application/json");
+		xhttp.send(JSON.stringify({fgt:e})); 
 		await sleep(1000);
 	}
 }
-self.addEventListener('message', demo(), false);
+self.onmessage = function(msg){
+	demo(msg.data);
+};
+// self.addEventListener('message', demo(), false);
--- resources/pages/wsgi_bin/datag.py
+++ resources/pages/wsgi_bin/datag.py
@@ -8,9 +8,12 @@ app = F.Flask(__name__)
 def post():
     import sys
     sys.path.insert(1, '/home/capstone/codebase')
+    import loginHandler as lh
     import settingsHandler as sh
-    data = {'voltage': 230, 'current': 5, 'nodename': sh.readSettings()[4], 'firmware':sh.readSettings()[5]}
-    return F.jsonify(data)
-
+    if (F.request.json != None and lh.isLogin(str(F.request.json.get('fgt')))):
+        data = {'voltage': 230, 'current': 5, 'nodename': sh.readSettings()[4], 'firmware':sh.readSettings()[5]}
+        return F.jsonify(data)
+    else:
+        return F.jsonify({'auth':'false'})
 if __name__ == "__main__":
     app.run()
