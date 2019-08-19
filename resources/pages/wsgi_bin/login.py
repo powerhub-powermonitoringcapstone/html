0a0c5c85c43d97acd2957ef2650bf9926fa660c7
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-19 11:26:54 +0800

    Massive rework of login and autologin frameworks, cleanup of code to follow. Also new password change method (needs to add fingerprint verification before changing password)

--- resources/pages/wsgi_bin/login.py
+++ resources/pages/wsgi_bin/login.py
@@ -19,7 +19,6 @@ def auth():
     sys.path.insert(1, '/home/capstone/codebase')
     import os, xml.etree.ElementTree as ET, loginHandler as lh, hashlib
     data = F.request.json
-    cwdf = '/home/capstone/codebase'
     sett = open(cwdf + '/pvt.xml', 'r')
     settings = ET.parse(sett)
     root = settings.getroot()
@@ -32,5 +31,30 @@ def auth():
         return ("True")
     else:
         return ("False")
+@app.route("/change/", methods=['GET', 'POST'])
+def change():
+    import sys, uuid, os, xml.etree.ElementTree as ET, hashlib
+    sys.path.insert(1, '/home/capstone/codebase')
+    import loginHandler as lh
+    lh.clear()
+    data = F.request.json
+    salt = str(uuid.uuid4())
+    auth = str(hashlib.sha256((data.get('auth') + salt).encode('utf-8')).hexdigest())
+    with open(cwdf + '/pvt.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        found = root.find("./private")
+        if (found == None):
+            root.append(ET.Element("private", {'key': auth, 'salt': salt}))
+        else:
+            found.set('key', auth)
+            found.set('salt', salt)
+        with open (cwdf + '/pvt.xml', 'wb') as settw:
+            settings.write(settw)
+            settw.close()
+    return (F.jsonify({\
+        'auth': auth,\
+        'salt': salt\
+        }))
 if __name__ == "__main__":
     app.run()
