9f8c3eb3efe8eac82cc5e7ea0fde56b42b88f575
Jericho Rejuso <clevergold6@gmail.com>
2019-09-05 07:54:37 +0800

    Optimized some functions.

---
+++ resources/pages/login.js
@@ -0,0 +1,18 @@
+function isLogin(fgt){
+	var x = 0;
+	if(fgt!=undefined){
+		$.ajax({
+		type: 'POST',
+		url: '/wsgi_bin/login/',
+		data: JSON.stringify ({fgt: fgt}),
+		success: function(data){
+			if (data != "True"){
+				Cookies.set('fgt',''); x = 0;
+			} else if (data == "True"){x = 1};
+		},
+		contentType: "application/json",
+		async: false
+		});
+	} else {return ("False"); x = 0;};
+	return x;
+}
